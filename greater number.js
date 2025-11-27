6a212c322d8fbf19ccd1c19ce214d46ce533a3ca
nirmitjr <nirmitkhatiwada07@gmail.com>
2025-11-27 16:17:34 +0545

    greater number file added

---
+++ greater number.js
@@ -0,0 +1,20 @@
+// WAP to find largest / smallest / equal number out of inputs
+
+let num1=Number(prompt("enter first number : "));
+let num2=Number(prompt("enter sec number : "));
+if (num1>num2){
+ console.log(`${num1} is greater than ${num2} by`, num1-num2 );
+
+}
+else if(num1<num2){
+    console.log(`${num2} is greater than ${num1} by`,num2-num1);
+}
+else{
+    console.log(`${num1} is equal to ${num2}`);
+} 
+    
+
+
+
+
+
