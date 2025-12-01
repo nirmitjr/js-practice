cc5f4060dee3dfc7ad9521ed0313492452ce7604
nirmitjr <nirmitkhatiwada07@gmail.com>
2025-12-01 12:09:08 +0545

    marks check

---
+++ marks check.js
@@ -0,0 +1,18 @@
+// wap to check marks
+let num=Number(prompt(`enter a number`));
+if ((num >100) || (num<0)){
+    console.log("Invalid number.");
+}
+ else if (num>=80){
+    console.log(`its grade A`)
+}
+else if (num>=60){
+    console.log(`its grade B`)
+}
+else if (num>=40){
+    console.log(`its grade C`)
+}
+else {
+    console.log(`fail`)
+}
+   
